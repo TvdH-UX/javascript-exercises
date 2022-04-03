4449166a41f7d6720ec398ca8670ac561650b0f8
TvdH-UX <thomasheuvel7@gmail.com>
2022-04-03 16:57:39 +0200

    Add function to remove value from array Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, value) {
+    console.log(array);
+    console.log(value);
+    if (array.includes(value)){
+        let location = array.indexOf(value);
+        array.splice(value - 1,1);
+    }
+    return array;
 };
 
 // Do not edit below this line
