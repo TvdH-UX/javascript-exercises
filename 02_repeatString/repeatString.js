0399a4d21b669b72213295c9ad16b6ebe9f9cc58
TvdH-UX <thomasheuvel7@gmail.com>
2022-04-03 15:58:19 +0200

    Add return "ERROR" when num < 0 Pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,12 @@
 const repeatString = function(string, num) {
     let result = "";
+    if (num < 0){
+        result = "ERROR";
+    } else {
     for (i = 0; i < num; i++){
         result += string;
     };
+    }
     return result;
 };
 
