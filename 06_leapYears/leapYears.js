9fd82dd96d6e811da097e39912ae7a6da80f754a
TvdH-UX <thomasheuvel7@gmail.com>
2022-04-04 17:55:18 +0200

    Filter out years divisible by 4 Filter out years divisible by both 100 & 400 Filter out years divisible by 100 and not 400 Return result

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -5,7 +5,18 @@
 // Return false if not leap year otherwise return true
 
 const leapYears = function(year) {
-
+    if (year % 4 == 0){
+        console.log("Year divisible by 4");
+        if (year % 100 == 0 && year % 400 == 0){
+            return true;
+        } else if (year % 100 == 0 && year % 400 != 0){
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
