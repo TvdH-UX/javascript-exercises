da2a510d6be5c066cbba350041742daa1f3991d2
TvdH-UX <thomasheuvel7@gmail.com>
2022-04-04 17:28:48 +0200

    Add psuedocode Add parameter to function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,10 @@
-const leapYears = function() {
+// Receive a year
+// Check whether a year is divisible by 4 (good)
+// Check whether a year is divisible by 100 (not good)
+// Check whether a year is divisible by both 100 and 400 (good)
+// Return false if not leap year otherwise return true
+
+const leapYears = function(year) {
 
 };
 
