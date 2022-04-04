d53f34ec194e9ef0c3dd671793f35bc15dd31f1b
TvdH-UX <thomasheuvel7@gmail.com>
2022-04-04 13:34:26 +0200

    Add support for first number being larger

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -8,7 +8,21 @@
 // Return result variable
 
 const sumAll = function(num1, num2) {
-    
+    sum = num1;
+    numbersBetween = [];
+    if (num1 < num2){
+        for (let i = num1 + 1; i <= num2; i++){
+            numbersBetween.push(i);
+        }
+    } else if (num1 > num2){
+        for (let i = num1 - 1; i >= num2; i--){
+            numbersBetween.push(i);
+        }
+    }
+    for (let n = 0; n < numbersBetween.length; n++){
+        sum += numbersBetween[n];
+    }
+    return sum;
 };
 
 // Do not edit below this line
