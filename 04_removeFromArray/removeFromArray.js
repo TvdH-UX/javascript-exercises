cce608a3a0ce6d1d7a3cf97b59773488de231160
TvdH-UX <thomasheuvel7@gmail.com>
2022-04-04 11:32:41 +0200

    Add functionality to remove multiple values from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,15 +2,30 @@
 // Receive value(s) which to remove from array
 // See which values need to be removed
 // Sift through the array looking for the values one by one
-// Splice the values out
+// Save the location
+// Splice the value out at saved location
+// Repeat
 // Return the trimmed array
 
-const removeFromArray = function(array, value) {
+// Code which only works for the first test
+// const removeFromArray = function(array, value) {
+//     if (array.includes(value)){
+//         let location = array.indexOf(value);
+//         array.splice(location ,1);
+//     }
+//     return array;
+// };
+
+// Code using rest parameters
+const removeFromArray = function(array, ...values) {
     console.log(array);
-    console.log(value);
-    if (array.includes(value)){
-        let location = array.indexOf(value);
-        array.splice(value - 1,1);
+    console.log(values);
+    for (let i = 0; i < values.length; i++){
+        if (array.includes(values[i])){
+            console.log(values[i]);
+            let location = array.indexOf(values[i]);
+            array.splice(location, 1);
+        }
     }
     return array;
 };
