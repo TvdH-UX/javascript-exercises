41b145e4ccdf188adde93234de4185b5d31b38c3
TvdH-UX <thomasheuvel7@gmail.com>
2022-04-04 12:57:03 +0200

    Add psuedocode for first test Add parameters to function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+// Receive two integers
+// Create result variable containing the first number
+// Create a variable with an empty array
+// Loop through the numbers until second number is reached
+// Put the numbers from the loop into the array
+// Loop through the array
+// Add numbers from array to result variable
+// Return result variable
 
+const sumAll = function(num1, num2) {
+    
 };
 
 // Do not edit below this line
