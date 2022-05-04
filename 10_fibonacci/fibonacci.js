891f3db7b290abe50b159b013fa2ca435a9d68be
TvdH-UX <thomasheuvel7@gmail.com>
2022-05-04 15:30:32 +0200

    Add fibonacci function, pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 0){
+        return "OOPS";
+    } else {
+        newArray = [];
+        for (let i = 0; i < number; i++){
+            if ( i < 2){
+                newArray.push(1);
+            } else {
+                newArray.push(newArray[newArray.length - 1] + newArray[newArray.length - 2]);
+                console.log(newArray);
+            }
+        }
+        return newArray[newArray.length - 1];
+    }
 };
 
 // Do not edit below this line
