38386e4cb8beb04171c1911539ba24dd967b9171
TvdH-UX <thomasheuvel7@gmail.com>
2022-05-04 14:24:58 +0200

    Check if string is palindrome, pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    newString = string.replace(/[^\w\s\']|_/g, "")
+                .replace(/\s+/g, "");
+    lowerCaseString = newString.toLowerCase();
+    trimmedString = lowerCaseString.trim();
+    newArray = Array.from(trimmedString);
+    reversedNewArray = newArray.slice().reverse();
+    for (let i = 0; i < newArray.length; i++){
+        if (newArray[i] !== reversedNewArray[i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
